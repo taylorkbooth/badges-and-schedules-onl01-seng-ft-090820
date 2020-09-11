9cfb8584c5ca8fc4f6061b5f8b3ab34de00b1afd
Taylor Booth <taylorkbooth@gmail.com>
2020-09-11 22:37:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,37 +2,32 @@ attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
   return "Hello, my name is #{name}."
-end
+end 
 
-
-
-def batch_badge_creator(array)
-  badges = []
-  array.each do |name|
-    badges.push("Hello, my name is #{name}.")
+def batch_badge_creator(attendees)
+  badges=[]
+  attendees.each do|badge|
+  badges.push("Hello, my name is #{badge}.")
   end
   return badges
-end
-
-
-
-def assign_rooms(array)
-  newarray = []
-  counter = 1
-  array.each do |name|
-    newarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
-    counter += 1
-  end
-  return nuarray
-end
-
-
-def printer(array)
-  batch_badge_creator(array).each do |id|
-    puts id
-  end 
   
-  assign_rooms(array).each do |id|
-    puts id 
+end 
+
+def assign_rooms(speakers)
+  assign=[]
+  
+  speakers.each_with_index{|speaker,index|
+  assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
+    }
+   return assign
+end 
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  
+  assign_rooms(attendees).each do |badge|
+    puts badge
   end
 end 
